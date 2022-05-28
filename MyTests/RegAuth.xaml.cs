f2dee010343d1a76ce6732e87ee5a52af81a7895
Terzor <79096834+Terzor@users.noreply.github.com>
2022-05-29 00:21:04 +0700

    Почти сделал авторизацию-регистрацию

---
+++ MyTests/RegAuth.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyTests
+{
+    public partial class RegAuth : Window
+    {
+        public RegAuth()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowRegistration(object sender, RoutedEventArgs e) //Регистрация
+        {
+            MainFrame.Content = new Registration();
+        }
+        private void WindowEntry(object sender, RoutedEventArgs e) //Авторизация
+        {
+            MainFrame.Content = new Authorization();
+        }
+    }
+}
---
+++ MyTests/Session.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTests
+{
+    public class Session
+    {
+        static public int UserId { get; set; }
+    }
+}
